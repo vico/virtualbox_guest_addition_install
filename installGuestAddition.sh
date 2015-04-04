b9d39186b4dd503f3becdef79c013f812906c43c
Tran Vinh Cuong <tranvinhcuong@gmail.com>
2015-04-04 12:46:30 +0900

    Initial commit

---
+++ installGuestAddition.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+version=$1
+
+if [ -z "$version" ]
+then
+  echo "Usage: $0 <version>"
+  echo "  Example: $0 4.3.26"
+  exit 1
+fi
+
+saved_dir=/tmp
+file_name=VBoxGuestAdditions_${version}.iso
+mount_dir=/media/VirtualBoxGuestAdditions
+vbox_url=http://download.virtualbox.org/virtualbox
+
+mkdir -p $mount_dir
+
+if [ ! -e $save_dir/$file_name ]
+then
+  wget -P $saved_dir $vbox_url/${version}/$file_name
+fi
+
+sudo mount -o loop,ro $file_name $mount_dir
+sudo sh $mount_dir/VBoxLinuxAdditions.run 
